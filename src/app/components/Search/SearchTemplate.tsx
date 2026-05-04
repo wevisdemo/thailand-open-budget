f9efffc087b2e9f07e41f5fb040832faeacfbeb2
petchill <songpon111333@gmail.com>
2026-05-04 20:54:01 +0700

    feat: add empty state of search

--- src/app/components/Search/SearchTemplate.tsx
+++ src/app/components/Search/SearchTemplate.tsx
@@ -9,6 +9,7 @@ import SummaryInfo from "@/app/components/Search/SearchBody/SummaryInfo";
 import SearchBodySkeleton from "@/app/components/Search/SearchBody/SearchBodySkeleton";
 import SearchIcon from "@/app/components/shared/icons/search-icon";
 import Dropdown, { type DropdownOption } from "../shared/Dropdown";
+import Image from "next/image";
 import BudgetVersionInfoModal from "./BudgetVersionInfoModal";
 import { useState } from "react";
 import type { Tag } from "@/types/search";
@@ -128,6 +129,34 @@ export default function SearchTemplate({ budgetData }: SearchTemplateProps) {
               <p className="mt-[20] font-serif text-[42px] font-bold text-white">
                 พิมพ์คีย์เวิร์ดที่สนใจในช่องค้นหาด้านบน
               </p>
+            </div>
+          </div>
+        ) : displayBudgetList.length === 0 ? (
+          <div className="relative">
+            <div className="flex w-full flex-col items-center justify-center">
+              <SearchBodySkeleton />
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center gap-[20px] bg-black/40">
+              <Image
+                src="/icons/search-not-found.svg"
+                alt="search not found"
+                width={36}
+                height={36}
+                className="mt-[96px]"
+              />
+              <p className="font-serif text-[42px] font-bold text-white">
+                ไม่พบข้อมูลที่เกี่ยวข้องกับ &apos;
+                {tags.map((t) => t.word).join(" ")}&apos;
+              </p>
+              <p className="text-[16px] text-white">
+                ลองใช้คำอื่นที่เกี่ยวข้อง
+              </p>
+              <button
+                onClick={() => setTags([])}
+                className="bg-interactive-01 px-[16px] py-[12px] font-bold text-white hover:cursor-pointer"
+              >
+                ล้างการค้นหา
+              </button>
             </div>
           </div>
         ) : (
